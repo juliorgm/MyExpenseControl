7b79ec05878ce9402f7e50a7da8bad3d97c9391c
Julio Gomes <juliorgm@gmail.com>
2019-02-27 14:36:11 -0300

    Commit inicial

---
+++ app/src/main/java/juliorgm/com/br/myexpensecontrol/MainActivity.java
@@ -0,0 +1,18 @@
+package juliorgm.com.br.myexpensecontrol;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
